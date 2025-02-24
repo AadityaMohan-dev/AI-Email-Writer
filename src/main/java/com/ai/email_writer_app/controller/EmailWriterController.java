6cffc115e585cd6db5bbae7e82ecffb979a2b9c0
Aaditya Mohan <aadityamohan@Aadityas-MacBook-Air.local>
2025-02-25 01:23:52 +0530

    updated

--- src/main/java/com/ai/email_writer_app/controller/EmailWriterController.java
+++ src/main/java/com/ai/email_writer_app/controller/EmailWriterController.java
@@ -4,13 +4,11 @@ import com.ai.email_writer_app.bo.EmailRequest;
 import com.ai.email_writer_app.service.EmailWriterSvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
+@CrossOrigin(origins = "*")
 public class EmailWriterController {
     @Autowired
     EmailWriterSvc emailWriterSvc;
